56d51333d73e53f6fd4d41577b68bb950d1f1554
Subrata Mondal <162795636+subratamondalnsec@users.noreply.github.com>
2024-11-02 18:34:10 +0530

    Update 25Minimum_Number_of_Taps_to_open_to_water_a_Garden_Leetcode-1326.cpp

--- 22Dynamic_Programming/class/25Minimum_Number_of_Taps_to_open_to_water_a_Garden_Leetcode-1326.cpp
+++ 22Dynamic_Programming/class/25Minimum_Number_of_Taps_to_open_to_water_a_Garden_Leetcode-1326.cpp
@@ -1,5 +1,76 @@
+/*
+Code BY Subrata Mondal
+problem link : https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
+*/
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {//greedy method  
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int>StartEnd(n+1,0);
+        for(int i=0;i<=n;i++){
+            int start=max(0,i-ranges[i]);
+            int end=min(n,i+ranges[i]);
+            StartEnd[start]=max(StartEnd[start],end);
+        }
+        int taps=0,maxEnd=0,currEnd=0;
+        for(int i=0;i<=n;i++){
+            if(i>maxEnd) return -1;
+            if(i>currEnd){
+                currEnd=maxEnd;
+                taps++;
+            }
+            maxEnd=max(maxEnd,StartEnd[i]);
+        }
+        return taps ;
+    }
+};
+
+//Approach-2 (Recursion + Memo)
+class Solution {  //Time Limit Exceeded 40 / 41 testcases passed
+public:
+    map<pair<int,int>,int> mp;
+    int N;
+    int solve(int i, int maxEnd, vector<pair<int,int>>& range) {
+        if(i >= range.size())
+            return maxEnd >= N ? 0 : 1e9;
+
+        if(range[i].first > maxEnd)
+            return 1e9;
+
+        if(mp.find({i,maxEnd}) != mp.end())
+            return mp[{i,maxEnd}];
+
+        int not_open_tap = solve(i+1, maxEnd , range);
+        
+        int open_tap     = 1 + solve(i+1, max(maxEnd, range[i].second), range);
+
+        return mp[{i, maxEnd}] = min(open_tap, not_open_tap); 
+    }
+    
+    int minTaps(int n, vector<int>& ranges) {
+        N = n;
+        
+        vector<pair<int,int>> range;
+
+        for(int i = 0; i < ranges.size(); i++) {
+            
+            int start = max(0, i - ranges[i]);
+            int end   = min(n, i + ranges[i]);
+            
+            range.push_back({start, end});
+            
+        }
+        
+        sort(range.begin(), range.end());
+        
+        int ans = solve(0, 0, range);
+        
+        return ans == 1e9 ? -1 : ans;
+    }
+};
+
 class Solution{
 public:
     int minTaps(int n,vector<int>&ranges){
