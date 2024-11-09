f08668348d26819cad7b167bec36d8120b94e1d1
Subrata Mondal <162795636+subratamondalnsec@users.noreply.github.com>
2024-11-09 12:07:02 +0530

    Add files via upload

---
+++ 23Graph/class/12directed_weighted_graph_adjacency_matrix.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+using namespace std;
+
+vector<vector<int> >uug;
+int v; // no of vertices
+void add_edge(int src,int dest,int wt){
+    uug[src][dest]=wt;
+}
+
+void display(){
+    for(int i=0;i<uug.size();i++){
+        for(auto el : uug[i]){
+            cout<<el <<"  ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    cin>>v;
+    int e;
+    cin>>e;
+    uug.resize(v,vector<int>(v,0));
+    while(e--){
+        int s,d,wt;
+        cin>>s>>d>>wt;
+        add_edge(s,d,wt);
+    }
+    display();
+}
+/*
+5 6
+0 1 4
+0 2 10
+1 2 7
+1 3 8
+2 4 4
+3 4 11
+*/
